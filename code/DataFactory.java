a2f7913e8395d987a4b27423c6331ab16b5ea1c9
hhhhqwq <2353912846@qq.com>
2025-10-10 17:22:36 +0800

    10.10 17:22

---
+++ code/DataFactory.java
@@ -0,0 +1,22 @@
+import java.lang.reflect.InvocationTargetException;
+
+public class DataFactory {
+    public DataManipulation createDataManipulation(String arg) {
+        String name;
+        if (arg.toLowerCase().contains("file")) {
+            name = "FileManipulation";
+        } else if (arg.toLowerCase().contains("postgresql")) {
+            name = "PostgresqlManipulation";
+        } else if (arg.toLowerCase().contains("opengauss")) {
+            name = "OpengaussManipulation";
+        } else {
+            throw new IllegalArgumentException("Illegal Argument:" + arg);
+        }
+        try {
+            return (DataManipulation) Class.forName(name).getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
